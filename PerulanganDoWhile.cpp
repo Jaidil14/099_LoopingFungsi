706de3c446c6d6230d4ec4f4f6312af05ddafe2c
Jaidil14 <jaidilrachmad@gmail.com>
2024-03-18 16:05:44 +0700

    Perulangan do while

---
+++ PerulanganDoWhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    char pilihan;
+do{
+    cout << "Berangkat mengambil takjil" << endl;
+    cout << "Antri takjil" << endl;
+    cout << "Ambil takjil" << endl;
+    cout << "Apakah mau antri kembali ?" << endl;
+
+}
+
+while(pilihan == 'y' || pilihan == 'Y');
+
+}
